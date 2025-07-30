96bf85a42af3e6936512a78b8d3ea648fe729302
Fredy Eleazar Cardona Banegas <104180831+FredyCB@users.noreply.github.com>
2025-07-29 19:01:50 -0600

    Update inventory_controller.py

--- controllers/inventory_controller.py
+++ controllers/inventory_controller.py
@@ -3,17 +3,21 @@ from bson import ObjectId
 from models.inventory import Inventory
 from utils.mongodb import get_collection
 
+#Creacion de la coleccion del inventario
 inventory_coll = get_collection("inventory")
 
+#funcion para agregar inventario
 async def add_inventory(item: Inventory) -> Inventory:
     item_dict = item.model_dump(exclude={"id"})
     inserted = inventory_coll.insert_one(item_dict)
     item.id = str(inserted.inserted_id)
     return item
 
+#funcion para consultar una parte o pieza del inventario
 async def get_inventory_by_part(part_id: str) -> list:
     return list(inventory_coll.find({"id_part": part_id}))
 
+#funcion para actualizar estado del inventario
 async def update_stock(inventory_id: str, quantity: int) -> dict:
     if not ObjectId.is_valid(inventory_id):
         raise HTTPException(400, "Invalid ID")
@@ -22,5 +26,6 @@ async def update_stock(inventory_id: str, quantity: int) -> dict:
         raise HTTPException(404, "Inventory item not found")
     return {"message": "Stock updated"}
 
+#funcion de alerta para stock bajo en inventario
 async def get_low_stock_items() -> list:
     return list(inventory_coll.find({"stock": {"$lt": 10}}))
