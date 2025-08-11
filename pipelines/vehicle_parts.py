52c48da8ae057277c65d405501687cf48ed069c9
Fredy Eleazar Cardona Banegas <104180831+FredyCB@users.noreply.github.com>
2025-08-10 18:57:19 -0600

    Add files via upload

---
+++ pipelines/vehicle_parts.py
@@ -0,0 +1,7 @@
+﻿def parts_by_vehicle_pipeline(vehicle_id):
+    return [
+        {"$match":{"vehicle_id": vehicle_id}},
+        {"$lookup":{"from":"catalog","localField":"part_id","foreignField":"_id","as":"part"}},
+        {"$unwind":"$part"},
+        {"$project":{"id":{"$toString":"$_id"},"part_id":1,"vehicle_id":1,"part.name":1,"part.price":1}}
+    ]
