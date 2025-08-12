196c0bc1976f7b5082e979460bfb7e1ce703e1d4
Fredy Eleazar Cardona Banegas <104180831+FredyCB@users.noreply.github.com>
2025-08-12 14:04:50 -0600

    Create __init__.py

---
+++ pipelines/__init__.py
@@ -0,0 +1,21 @@
+"""
+Módulo de pipelines para operaciones avanzadas en el almacén de autopartes
+"""
+
+from .inventory import (
+    pipeline_inventory_with_part
+)
+
+from .vehicle_parts import (
+    pipeline_parts_by_vehicle,
+    pipeline_vehicles_by_part
+)
+
+__all__ = [
+    # Pipelines de inventario
+    "pipeline_inventory_with_part",
+
+    # Pipelines de compatibilidad vehículo-parte
+    "pipeline_parts_by_vehicle",
+    "pipeline_vehicles_by_part"
+]
