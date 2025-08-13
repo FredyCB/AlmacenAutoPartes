3fb3240ddbcd8001c32f4658632e360041a68173
Fredy Eleazar Cardona Banegas <104180831+FredyCB@users.noreply.github.com>
2025-08-13 17:28:43 -0600

    Delete models directory

--- models/compatibilty.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Optional
-
-class Compatibility(BaseModel):
-    id: Optional[str] = Field(default=None)
-    id_part: str
-    id_vehicle: str
